c79b8f7d8d81671a3f6046f2c02cbd227ff0d215
valencia <cinthihavi@gmail.com>
2024-11-21 10:47:43 -0800

    feat: - implemented router - created home and about pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home: React.FC = () => {
+    return (
+       <>
+        <h2>Home Page!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nobis fuga voluptates magnam saepe corrupti, fugit veritatis. Minus esse, sunt cumque cupiditate, tempora et corporis exercitationem perferendis quibusdam repellendus nostrum!
+        Nesciunt repudiandae est dicta? Optio quasi aliquam illum alias, error veniam dicta numquam doloremque ratione, officia nostrum accusamus perferendis rem corporis enim nesciunt illo. Exercitationem autem quisquam qui nemo neque?
+        Hic doloribus necessitatibus distinctio repudiandae tempora vel aliquam nam quo sed soluta expedita consequuntur vero, est et qui aperiam aut maxime officia, ad sunt sequi incidunt eaque molestiae. Porro, maiores.</p></>
+    )
+}
+
+export default Home;
