4d82e5309c39b3bccca752f8b732044a20a5f3b0
Leonardo Eguino <releegva@gmail.com>
2024-11-24 17:07:47 -0400

    feat: implemented app navbar (not working)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,144 @@
 import { Link } from "react-router-dom";
 import navbarRoutes from "../config/navbar-paths";
+import { MouseEvent, useState } from "react";
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import { TITLE } from "../config/strings";
 
 const Navbar: React.FC = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <nav>
-      <ul>
-        {navbarRoutes.map((route) => (
-          <li>
-            <Link key={route.text} to={route.path}>
-              {route.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    // <nav>
+    //   <ul>
+    //     {navbarRoutes.map((route) => (
+    //       <li>
+    //         <Link key={route.text} to={route.path}>
+    //           {route.text}
+    //         </Link>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </nav>
+    <AppBar position="static" sx={{ mb: 2.5 }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon
+            sx={{
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+              color: "inherit",
+            }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            {TITLE}
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {navbarRoutes.map((route) => (
+                <MenuItem key={route.text} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: "center" }}>
+                    {route.text}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            {TITLE}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {navbarRoutes.map((route) => (
+              <Button
+                key={route.text}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "inherit", display: "block" }}
+              >
+                {route.text}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
---
+++ src/config/strings.ts
@@ -0,0 +1,1 @@
+export const TITLE = "IS FINAL PROJECT (SIG)";
